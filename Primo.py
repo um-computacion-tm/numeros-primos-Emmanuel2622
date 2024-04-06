09cca4028a37b8634765f5cc6df8f4a89597acf3
emmaa.den <emma26228@gmail.com>
2024-04-06 15:38:27 -0300

    Primo.py

--- Primo.py
+++ Primo.py
@@ -1,5 +1,7 @@
 def is_primo(value):
-    div = 2 
+    div = 2
+    if value == 1:
+        return False
     while div < value:
         if value % div == 0:
             return False
--- Test.py
+++ Test.py
@@ -4,7 +4,7 @@ import unittest
 class TestPrimos(unittest.TestCase):
     def test_1(self):
         result = is_primo(1)
-        self.assertEqual(result, True)
+        self.assertEqual(result, False)
 
     def test_2(self):
         result = is_primo(2)
